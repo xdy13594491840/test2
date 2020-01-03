c066e1e5d811e8494f371c7caec75ed2bae822b6
000 <000@000-PC>
2020-01-03 10:59:32 +0800

    第三次

--- test2/src/test2/test2.java
+++ test2/src/test2/test2.java
@@ -3,5 +3,8 @@ package test2;
 public class test2 {
 	public static void main(String[] args) {
 		System.out.println("123456");
+		System.out.println("4546");
+		System.out.println("78");
+		System.out.println("963");
 	}
 }
